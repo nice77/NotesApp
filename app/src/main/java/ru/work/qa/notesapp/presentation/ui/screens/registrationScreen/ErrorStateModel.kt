1ed6623f15d5f04a028b4edc075b56d982c10e9d
R.Faskhetdinov <rrfaskhetdinov@detmir.ru>
2024-04-11 00:26:05 +0300

    Unused class deleted

--- app/src/main/java/ru/work/qa/notesapp/presentation/ui/screens/registrationScreen/ErrorStateModel.kt
+++
@@ -1,7 +0,0 @@
-package ru.work.qa.notesapp.presentation.ui.screens.registrationScreen
-
-data class ErrorStateModel(
-    val isWrongEmail: Boolean = false,
-    val isWrongPassword: Boolean = false,
-    val isUserDataIncorrect: Boolean = false,
-)
